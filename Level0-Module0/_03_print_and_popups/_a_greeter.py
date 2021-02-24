9233c27f869fff4fc3d844959c1196e452741e9f
Ronald Jenkins <rrizonj@Gmail.com>
2021-02-23 16:32:00 -0800

    I finshed greeter and started mad libs

--- Level0-Module0/_03_print_and_popups/_a_greeter.py
+++ Level0-Module0/_03_print_and_popups/_a_greeter.py
@@ -6,16 +6,15 @@ if __name__ == '__main__':
     # Make a new window variable, window = Tk()
     window = Tk()
     # Hide the window using the window's .withdraw() method
-    Tk.withdraw()
+    window.withdraw()
     # Ask the user for their name and save it to a variable
     # name = simpledialog.askstring(title='Greeter', prompt="What is your name?")
-    name = simpledialog.askstring("What is your name?")
+    name = simpledialog.askstring("Greeter", "What is your name?")
     # Show a message box with your message using the .showinfo() method
-    Tk.showinfo()
+    messagebox.showinfo("Greeter", name)
     # Print your message to the console using the print() function
-    Tk.print()
+    print(name)
     # Show an error message using messagebox.showerror()
     messagebox.showerror()
     # Run the window's .mainloop() method
-    Tk.mainloop()
     pass
