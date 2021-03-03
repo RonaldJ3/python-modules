6a25be77258b4d7963caf8f44355acce81b3e92a
Ronald Jenkins <rrizonj@Gmail.com>
2021-03-02 16:59:36 -0800

    I finshed mad libs and started world domination.

--- Level0-Module0/_03_print_and_popups/_b_mad_libs.py
+++ Level0-Module0/_03_print_and_popups/_b_mad_libs.py
@@ -8,17 +8,17 @@ if __name__ == '__main__':
     window.withdraw()
     # Put this sentence in a pop-up message box:
     # "If you find yourself having to cross a piranha-infested river, here's how to do it..."
-    libs = simplemessage.askstring("", "If you find yourself having to cross a piranha-infested river, here's how to do it...")
+    libs = simpledialog.askstring("mad libs", "If you find yourself having to cross a piranha-infested river, here's how to do it...")
     # Get the player to enter an adjective
-    messagebox.showinfo("", libs)
+    adjective = simpledialog.askstring("mad libs","type a adjective")
     # Get the player to enter a type of liquid
-
+    liqiud = simpledialog.askstring("mad libs","type the name of a liqud")
     # Get the player to enter a body part
-
+    bodypart = simpledialog.askstring("mad libs","type the name of a body part")
     # Get the player to enter a verb
-
+    verb = simpledialog.askstring("mad libs","type a verb")
     # Get the player to enter a place
-
+    place = simpledialog.askstring("mad libs","type a place")
     # The story below has has been written as a group of Strings joined
     # together by + signs. The story contains place holders, indicated
     # by [** **] which you need to replace with the values entered by the
@@ -27,17 +27,17 @@ if __name__ == '__main__':
     #       other parts of the story.
 
     story = (
-        "Piranhas are more [**adjective**] during the day, so cross the river at\n"
-        "night. Piranhas are attracted to fresh [**type of liquid**] and will most\n"
-        "likely take a bite out of your [**body part**] if you [**verb**]. Whatever\n"
+        "Piranhas are more " + adjective + " during the day, so cross the river at\n"
+        "night. Piranhas are attracted to fresh " + liqiud + " and will most\n"
+        "likely take a bite out of your " + bodypart + " if you " +verb+ ". Whatever\n"
         "you do, if you have an open wound, try to find another way to get "
-        "back to the [**place**]. Good luck!"
+        "back to the " +place+". Good luck!"
     )
 
     # Make a pop-up that contains the final story. The \n escape characters add
     # line breaks to the story. If you need to, move them around to make your
     # story look better in the pop-up
-
+    simpledialog.askstring("",story)
     # If you want to write your own Madlib story, just change the story variable
     # and ask the player different questions.
 
--- Level0-Module0/_03_print_and_popups/_c_world_domination.py
+++ Level0-Module0/_03_print_and_popups/_c_world_domination.py
@@ -3,13 +3,13 @@ from tkinter import messagebox, simpledialog, Tk
 # Create an if-main code block, *hint, type main then ctrl+space to auto-complete
     
     # Make a new window variable, window = Tk()
-    
+    window = Tk()
     # Hide the window using the window's .withdraw() method
-    
+    Tk.withdraw()
     # 1. Ask the user if they know how to write code.
-    
+    dom = simpledialog.askstring("coder","do you know how to write code?")
     # 2. If they say "yes", tell them they will rule the world in a message box pop-up.
-
+    if
     # 3. Otherwise, tell them to sign up for classes at The League in an error box pop-up.
     
     # Run the window's .mainloop() method
