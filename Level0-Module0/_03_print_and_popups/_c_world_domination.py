8b808c779c44b77ac9a2411b0d13cb966c7f2baa
Ronald Jenkins <rrizonj@Gmail.com>
2021-03-09 17:06:33 -0800

    I finshed world domination and quiz game and started awesome or not

--- Level0-Module0/_03_print_and_popups/_c_world_domination.py
+++ Level0-Module0/_03_print_and_popups/_c_world_domination.py
@@ -1,15 +1,18 @@
 from tkinter import messagebox, simpledialog, Tk
 
 # Create an if-main code block, *hint, type main then ctrl+space to auto-complete
-    
+if __name__ == '__main__':
+
     # Make a new window variable, window = Tk()
     window = Tk()
     # Hide the window using the window's .withdraw() method
-    Tk.withdraw()
+    window.withdraw()
     # 1. Ask the user if they know how to write code.
-    dom = simpledialog.askstring("coder","do you know how to write code?")
+    dom = simpledialog.askstring("", "do you know how to write code?")
     # 2. If they say "yes", tell them they will rule the world in a message box pop-up.
-    if
+    if dom.lower() == 'yes':
+        messagebox.showinfo('', 'You will rule the world!')
     # 3. Otherwise, tell them to sign up for classes at The League in an error box pop-up.
-    
+    elif dom.lower() == 'no':
+        messagebox.showerror('', 'You should sign up for classes at the league!')
     # Run the window's .mainloop() method
--- Level0-Module0/_03_print_and_popups/_d_quiz_game.py
+++ Level0-Module0/_03_print_and_popups/_d_quiz_game.py
@@ -1,21 +1,33 @@
 from tkinter import messagebox, simpledialog, Tk
 
 # Create an if-main code block, *hint, type main then ctrl+space to auto-complete
-    
-    # Make a new window variable, window = Tk()
-    
-    # Hide the window using the window's .withdraw() method
-    
-    # 1. Create a variable to hold the user's score. Set it equal to zero. 
+if __name__ == '__main__':
 
+    # Make a new window variable, window = Tk()
+    window = Tk()
+    # Hide the window using the window's .withdraw() method
+    window.withdraw()
+    # 1. Create a variable to hold the user's score. Set it equal to zero. 
+    int = 0
     # ASK A QUESTION AND CHECK THE ANSWER
 
-    #      // 2.  Ask the user a question 
-    
+    #      // 2.  Ask the user a question
+    Q = simpledialog.askstring('', 'What is 2876 + 4328 ?')
     #      // 3.  Use an if statement to check if their answer is correct
-
+    if Q.lower() == '7104':
+        int += 1
+    q = simpledialog.askstring('', 'What color is the sun?')
+    if q.lower() == 'yellow':
+        int += 1
+    A = simpledialog.askstring('', 'What is the summer azimuth of the sunset?')
+    if A.lower() == '300.15':
+        int += 1
+    a = simpledialog.askstring('', 'what is the largest human organ?')
+    if a.lower() == 'skin':
+        int += 1
+    messagebox.showinfo('', 'Your score is'+ str(int) +'congrats')
     #      // 4.  if the user's answer was correct, add one to their score 
- 
+
     # MAKE MORE QUESTIONS. Ask more questions by repeating the above 
     #      // Option: Subtract a point from their score for a wrong answer
  
